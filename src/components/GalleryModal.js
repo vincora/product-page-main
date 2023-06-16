2c8c32b80b1cc870835a6dd68907049fe9860b35
Yulia Glagola <glagolajulia@gmail.com>
2023-06-16 22:25:20 +0400

    arrows and close icon as inline svg

--- src/components/GalleryModal.js
+++ src/components/GalleryModal.js
@@ -1,8 +1,6 @@
 import style from "./GalleryModal.module.scss";
 import { useState, useRef } from "react";
 import { product } from "../ItemInfo";
-import iconPrevious from "../images/icon-previous.svg";
-import iconNext from "../images/icon-next.svg";
 import cn from "classnames";
 import { useClickAway } from "react-use";
 import { useDispatch, useSelector } from "react-redux";
@@ -32,25 +30,26 @@ const GalleryModal = () => {
       })}
     >
       <div className={style.layout} ref={ref}>
+        <div className={style.iconClose} onClick={closeModal}>
+          <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg"><path d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z" fill="#fff" fillRule="evenodd"/></svg>
+        </div>
         <div className={style.currentPicWrapper}>
-          <img src={currentPicture} className={style.currentPic} />
-          <div
-            className={cn(style.arrow, style.arrowPrevious)}
+          <div className={cn(style.arrow, style.arrowPrevious)}
             onClick={() => {
               setItemNumber((current) =>
                 current > 0 ? current - 1 : pictures.length - 1
               );
-            }}
-          >
-            <img src={iconPrevious} alt="previous" />
+            }}>
+            <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg"><path d="M11 1 3 9l8 8" stroke="#1D2026" stroke-width="3" fill="none" fillRule="evenodd"/></svg>
           </div>
+          <img src={currentPicture} className={style.currentPic} />
           <div
             className={cn(style.arrow, style.arrowNext)}
             onClick={() => {
               setItemNumber((current) => current + 1);
             }}
           >
-            <img src={iconNext} alt="next" />
+            <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg"><path d="m2 1 8 8-8 8" stroke="#1D2026" stroke-width="3" fill="none" fillRule="evenodd"/></svg>
           </div>
         </div>
         <div className={style.thumbnails}>
